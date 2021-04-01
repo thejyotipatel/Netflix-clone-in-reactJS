ec53296c1d04b651bb6ededa35bed223b5699836
jyoti patel <jyotipatelfyit77100@gmail.com>
2021-03-31 19:42:28 -0700

    added style in list

--- src/ToDoApp/ToDoData.js
+++ src/ToDoApp/ToDoData.js
@@ -1,28 +1,28 @@
 const todoData = [
     {
-        id: 1,
-        text: "Take out the trash",
-        completed: true
-      },
-      {
-        id: 2,
-        text: "Grocery shopping",
-        completed: false
-      },
-      {
-        id: 3,
-        text: "Wash clothes",
-        completed: false
-      },
-      {
-        id: 4,
-        text: "Clean my room",
-        completed: true
-      },
-      {
-        id: 5,
-        text: " Post video on YouTube",
-        completed: false
-      }
+      id: 1,
+      text: "Take out the trash",
+      completed: true
+    },
+    {
+      id: 2,
+      text: "Grocery shopping",
+      completed: false
+    },
+    {
+      id: 3,
+      text: "Wash clothes",
+      completed: false
+    },
+    {
+      id: 4,
+      text: "Clean my room",
+      completed: true
+    },
+    {
+      id: 5,
+      text: " Post video on YouTube",
+      completed: false
+    }
 ]
 export default todoData
